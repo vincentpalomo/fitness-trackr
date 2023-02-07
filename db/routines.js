3e22e0d9fb033df2431b507c299d1a8945616b20
Vincent Palomo <vincentpalomo@yahoo.com>
2023-02-06 23:55:30 -0600

    updated routine db

--- db/routines.js
+++ db/routines.js
@@ -142,11 +142,17 @@ async function updateRoutine({ id, ...fields }) {
 
 async function destroyRoutine(id) {
   try {
-    await client.query(`
+    await client.query(
+      `
     DELETE FROM routine_activities
-    `);
+    WHERE "routineId" = $1
+    `,
+      [id]
+    );
 
-    const { rows: routine } = await client.query(
+    const {
+      rows: [routine],
+    } = await client.query(
       `
     DELETE FROM routines
     WHERE id = $1
