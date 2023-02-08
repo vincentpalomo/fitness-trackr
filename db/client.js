71817c5451e1c9aea25b89f95e6c0fd1babc26b4
vincent <vincentpalomo@yahoo.com>
2023-02-07 22:04:37 -0600

    potential bug in updateRoutine

--- db/client.js
+++ db/client.js
@@ -1,8 +1,7 @@
 const { Pool } = require('pg');
 
 const connectionString =
-  process.env.DATABASE_URL ||
-  'postgresql://vincentpalomo:v2_3ysed_nbsdWVz5vhCJymkwPH3dqmN@db.bit.io:5432/vincentpalomo/fitness-dev?ssl=true';
+  process.env.DATABASE_URL || 'https://localhost:5432/fitness-dev';
 
 const client = new Pool({
   connectionString,
