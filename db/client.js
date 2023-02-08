c86fa92870f20c28bd7efef4d6b998d81076f797
vincent <vincentpalomo@yahoo.com>
2023-02-07 21:55:38 -0600

    updated github actions with online db for fitness

--- db/client.js
+++ db/client.js
@@ -1,10 +1,15 @@
 const { Pool } = require('pg');
 
-const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/fitness-dev';
+const connectionString =
+  process.env.DATABASE_URL ||
+  'postgresql://vincentpalomo:v2_3ysed_nbsdWVz5vhCJymkwPH3dqmN@db.bit.io:5432/vincentpalomo/fitness-dev?ssl=true';
 
 const client = new Pool({
   connectionString,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : undefined,
 });
 
 module.exports = client;
